ebe7c4f2e7f01fa4ca48a64e12e9446ad1c9c448
Arya Augusta <aryaaugusta@gmail.com>
2025-06-04 14:04:18 +0700

    change image in services pages

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -242,7 +242,7 @@ const Services = () => {
                                         </div>
                                         <div className="md:order-1 bg-white dark:bg-navy-700 p-6 rounded-lg shadow-md">
                                             <img
-                                                src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80"
+                                                src="/images/actuarial-support.jpg"
                                                 alt="Ongoing support"
                                                 className="rounded-lg"
                                             />
